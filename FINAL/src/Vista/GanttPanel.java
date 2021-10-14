e95ef2f57e6cbd3ad1194d43574ce3f70a3e36ef
EstebanOG <juanesteban199911@gmail.com>
2021-10-13 21:39:34 -0500

    Initial commit

---
+++ FINAL/src/Vista/GanttPanel.java
@@ -0,0 +1,45 @@
+package Vista;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class GanttPanel extends TablePanel {
+
+	private TitledBorder titledBorder;
+	private TableCellRedendererColor cellRenderer;
+
+	public GanttPanel(JFrame window, String title) {
+		super(window, title);
+		titledBorder = (TitledBorder) getBorder();
+		getTable().setShowGrid(false);
+		getTable().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		setTableModel(new DefaultTableModel(new Object[] { "P" }, 0));
+		cellRenderer = new TableCellRedendererColor();
+		cellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+		getTable().setDefaultRenderer(Object.class, cellRenderer);
+		setInitialColumns(100);
+	}
+
+	public void setInitialColumns(int max) {
+		for (int i = 1; i <= max; i++) {
+			getTableModel().addColumn(i);
+		}
+	}
+
+	public void updateTitleColor(Boolean isAvalible) {
+		if (isAvalible) {
+			titledBorder.setTitleColor(Color.green);
+			repaint();
+		} else {
+			titledBorder.setTitleColor(Color.red);
+			repaint();
+
+		}
+	}
+}
